a7fa053fed346d14611ca26f91f01e8742a28acc
Nathan Ford <NathanFord68@gmail.com>
2020-02-01 17:38:34 -0700

    added cloudtestapp ddl

---
+++ cloudtestapp.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:54293
+-- Generation Time: Feb 01, 2020 at 04:35 PM
+-- Server version: 5.7.9
+-- PHP Version: 5.6.40
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cloudtestapp`
+--
+DROP DATABASE IF EXISTS `cloudtestapp`;
+CREATE SCHEMA `cloudtestapp`;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `USER_ID` int(11) NOT NULL,
+  `FIRST_NAME` varchar(20) NOT NULL,
+  `LAST_NAME` varchar(20) NOT NULL,
+  `EMAIL` varchar(100) NOT NULL,
+  `USERNAME` varchar(20) NOT NULL,
+  `PASSWORD` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`USER_ID`),
+  ADD UNIQUE KEY `USERNAME_UNIQUE` (`USERNAME`),
+  ADD UNIQUE KEY `EMAIL_UNIQUE` (`EMAIL`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `USER_ID` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
