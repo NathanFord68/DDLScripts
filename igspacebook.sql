b0433ac3c53b6dc88eaa6fa23ee8d6f6251e88b3
Nathan Ford <NathanFord68@gmail.com>
2020-02-06 00:12:47 -0700

    igspacebook update to accomodate suspended users. View updated with the tables

--- igspacebook.sql
+++ igspacebook.sql
@@ -1,162 +1,126 @@
--- phpMyAdmin SQL Dump
--- version 4.7.0
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: Feb 04, 2020 at 04:13 AM
--- Server version: 5.6.34-log
--- PHP Version: 7.1.5
+-- MySQL Workbench Forward Engineering
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema igspacebook
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `igspacebook` ;
+
+-- -----------------------------------------------------
+-- Schema igspacebook
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `igspacebook` DEFAULT CHARACTER SET latin1 ;
+USE `igspacebook` ;
+
+-- -----------------------------------------------------
+-- Table `igspacebook`.`user_role`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `igspacebook`.`user_role` ;
+
+CREATE TABLE IF NOT EXISTS `igspacebook`.`user_role` (
+  `user_role_id` TINYINT(4) NOT NULL COMMENT 'Primary key',
+  `role_desc` VARCHAR(20) NOT NULL COMMENT 'A description of the alien user\'s role in the application',
+  PRIMARY KEY (`user_role_id`),
+  UNIQUE INDEX `role_desc` (`role_desc` ASC))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_bin;
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+-- -----------------------------------------------------
+-- Table `igspacebook`.`user`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `igspacebook`.`user` ;
 
---
--- Database: `igspacebook`
---
-CREATE DATABASE `igspacebook`;
+CREATE TABLE IF NOT EXISTS `igspacebook`.`user` (
+  `user_id` BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT 'Primary key',
+  `date_created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'The date the user was created (e.g. registered)',
+  `full_name` VARCHAR(75) NOT NULL COMMENT 'The real, full name of the alien user',
+  `email` VARCHAR(255) NOT NULL COMMENT 'The email address which uniquely identifies the alien user on the application',
+  `passwd` CHAR(64) NOT NULL COMMENT 'The hashed password of the user',
+  `salt` CHAR(64) NOT NULL COMMENT 'The salt used to hash the password',
+  `user_role_id` TINYINT(4) NOT NULL DEFAULT '1' COMMENT 'Foreign key constraint referencing user_role.user_role_id',
+  PRIMARY KEY (`user_id`),
+  UNIQUE INDEX `email` (`email` ASC),
+  INDEX `user_fk_user_role` (`user_role_id` ASC),
+  CONSTRAINT `user_fk_user_role`
+    FOREIGN KEY (`user_role_id`)
+    REFERENCES `igspacebook`.`user_role` (`user_role_id`)
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+AUTO_INCREMENT = 24
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `igspacebook`.`suspension`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `igspacebook`.`suspension` ;
+
+CREATE TABLE IF NOT EXISTS `igspacebook`.`suspension` (
+  `suspension_id` BIGINT(20) NOT NULL,
+  `user_id` BIGINT(20) NOT NULL,
+  `status` TINYINT(4) NOT NULL,
+  `suspension_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`suspension_id`),
+  UNIQUE INDEX `user_id` (`user_id` ASC),
+  CONSTRAINT `suspension_fk_user`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `igspacebook`.`user` (`user_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
+
+
+-- -----------------------------------------------------
+-- Table `igspacebook`.`user_profile`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `igspacebook`.`user_profile` ;
+
+CREATE TABLE IF NOT EXISTS `igspacebook`.`user_profile` (
+  `user_id` BIGINT(20) NOT NULL COMMENT 'Primary key. Foreign key reference to user.user_id',
+  `display_name` VARCHAR(50) NOT NULL COMMENT 'The display name, or alias, of the alien user',
+  `tagline` VARCHAR(75) NULL DEFAULT NULL COMMENT '(Optional) A short tagline that summarizes the user',
+  `bio` VARCHAR(1000) NULL DEFAULT NULL COMMENT '(Optional) A longer description, or biography, about the user',
+  `profile_photo` BLOB NULL DEFAULT NULL COMMENT '(Optional) A photo of the user',
+  `galaxy_name` VARCHAR(50) NULL DEFAULT NULL COMMENT '(Optional) Which galaxy the user is from',
+  `solar_sys_name` VARCHAR(50) NULL DEFAULT NULL COMMENT '(Optional) Which solar system the user is from',
+  `planet_name` VARCHAR(50) NULL DEFAULT NULL COMMENT '(Optional) Which planet the user is from',
+  PRIMARY KEY (`user_id`),
+  UNIQUE INDEX `display_name` (`display_name` ASC),
+  INDEX `galaxy_name` (`galaxy_name` ASC),
+  INDEX `solar_sys_name` (`solar_sys_name` ASC),
+  INDEX `planet_name` (`planet_name` ASC),
+  INDEX `galaxy_name_2` (`galaxy_name` ASC, `solar_sys_name` ASC),
+  INDEX `galaxy_name_3` (`galaxy_name` ASC, `solar_sys_name` ASC, `planet_name` ASC),
+  CONSTRAINT `user_profile_fk_user`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `igspacebook`.`user` (`user_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+USE `igspacebook` ;
+
+-- -----------------------------------------------------
+-- Placeholder table for view `igspacebook`.`get_profile`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `igspacebook`.`get_profile` (`user_id` INT, `date_created` INT, `full_name` INT, `email` INT, `passwd` INT, `salt` INT, `user_role_id` INT, `display_name` INT, `tagline` INT, `bio` INT, `profile_photo` INT, `galaxy_name` INT, `solar_sys_name` INT, `planet_name` INT, `suspension_status` INT, `suspension_date` INT);
+
+-- -----------------------------------------------------
+-- View `igspacebook`.`get_profile`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `igspacebook`.`get_profile`;
+DROP VIEW IF EXISTS `igspacebook`.`get_profile` ;
 USE `igspacebook`;
--- --------------------------------------------------------
+CREATE  OR REPLACE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `igspacebook`.`get_profile` AS select `igspacebook`.`user`.`user_id` AS `user_id`,`igspacebook`.`user`.`date_created` AS `date_created`,`igspacebook`.`user`.`full_name` AS `full_name`,`igspacebook`.`user`.`email` AS `email`,`igspacebook`.`user`.`passwd` AS `passwd`,`igspacebook`.`user`.`salt` AS `salt`,`igspacebook`.`user`.`user_role_id` AS `user_role_id`,`igspacebook`.`user_profile`.`display_name` AS `display_name`,`igspacebook`.`user_profile`.`tagline` AS `tagline`,`igspacebook`.`user_profile`.`bio` AS `bio`,`igspacebook`.`user_profile`.`profile_photo` AS `profile_photo`,`igspacebook`.`user_profile`.`galaxy_name` AS `galaxy_name`,`igspacebook`.`user_profile`.`solar_sys_name` AS `solar_sys_name`,`igspacebook`.`user_profile`.`planet_name` AS `planet_name`,`igspacebook`.`suspension`.`status` AS `suspension_status`,`igspacebook`.`suspension`.`suspension_date` AS `suspension_date` from ((`igspacebook`.`user` join `igspacebook`.`user_profile` on((`igspacebook`.`user`.`user_id` = `igspacebook`.`user_profile`.`user_id`))) left join `igspacebook`.`suspension` on((`igspacebook`.`user`.`user_id` = `igspacebook`.`suspension`.`user_id`)));
 
---
--- Table structure for table `user`
---
-
-CREATE TABLE `user` (
-  `user_id` bigint(20) NOT NULL COMMENT 'Primary key',
-  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'The date the user was created (e.g. registered)',
-  `full_name` varchar(75) NOT NULL COMMENT 'The real, full name of the alien user',
-  `email` varchar(255) NOT NULL COMMENT 'The email address which uniquely identifies the alien user on the application',
-  `passwd` char(64) NOT NULL COMMENT 'The hashed password of the user',
-  `salt` char(64) NOT NULL COMMENT 'The salt used to hash the password',
-  `user_role_id` tinyint(4) NOT NULL DEFAULT '1' COMMENT 'Foreign key constraint referencing user_role.user_role_id'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`user_id`, `date_created`, `full_name`, `email`, `passwd`, `salt`, `user_role_id`) VALUES
-(17, '2020-02-01 19:19:32', 'Michael Mason', 'mmason.pro@gmail.com', '9238c87224d8a42b802d312a90e750f380725eaa4a3742245d44a0420b41eb6a', 'bM8XebKFCuAZuSU3wfRBN3RqYGAIXJsEduWmIDiw3S38VzBqQkPGbbyrseuotITZ', 1),
-(21, '2020-02-01 19:39:43', 'Michael mason', 'MMason29@my.gcu.edu', '3cffb257735766a16362c8d139a7925375ffe6f6a0267375096816e0d0e8bd4b', '935P0B2HytHVbNvQxWmkT1gjxWlL6uVkjF0dLETpZbeiF4a3uxAh9u2H8HCpd8Ol', 1),
-(22, '2020-02-03 17:54:13', 'Mike Mason', 'djsavra@gmail.com', '43a4980d46c01009c5072d61f131de3f85e878928cbeadb87b978a4798288bed', 'MT3wIgp8erN4e8Jn61UK6bKBSP96r7t5F6dgtohrWnnjy4DVrQWQ51R5LS8rFQKT', 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user_profile`
---
-
-CREATE TABLE `user_profile` (
-  `user_id` bigint(20) NOT NULL COMMENT 'Primary key. Foreign key reference to user.user_id',
-  `display_name` varchar(50) NOT NULL COMMENT 'The display name, or alias, of the alien user',
-  `tagline` varchar(75) DEFAULT NULL COMMENT '(Optional) A short tagline that summarizes the user',
-  `bio` varchar(1000) DEFAULT NULL COMMENT '(Optional) A longer description, or biography, about the user',
-  `profile_photo` blob COMMENT '(Optional) A photo of the user',
-  `galaxy_name` varchar(50) DEFAULT NULL COMMENT '(Optional) Which galaxy the user is from',
-  `solar_sys_name` varchar(50) DEFAULT NULL COMMENT '(Optional) Which solar system the user is from',
-  `planet_name` varchar(50) DEFAULT NULL COMMENT '(Optional) Which planet the user is from'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `user_profile`
---
-
-INSERT INTO `user_profile` (`user_id`, `display_name`, `tagline`, `bio`, `profile_photo`, `galaxy_name`, `solar_sys_name`, `planet_name`) VALUES
-(21, 'Xergii the Astrol', NULL, NULL, NULL, NULL, NULL, NULL),
-(22, 'Chromes', NULL, NULL, NULL, NULL, NULL, NULL);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user_role`
---
-
-CREATE TABLE `user_role` (
-  `user_role_id` tinyint(4) NOT NULL COMMENT 'Primary key',
-  `role_desc` varchar(20) COLLATE utf8_bin NOT NULL COMMENT 'A description of the alien user''s role in the application'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
---
--- Dumping data for table `user_role`
---
-
-INSERT INTO `user_role` (`user_role_id`, `role_desc`) VALUES
-(4, 'Master Admin'),
-(2, 'Moderator'),
-(3, 'Site Admin'),
-(1, 'User');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`user_id`),
-  ADD UNIQUE KEY `email` (`email`),
-  ADD KEY `user_fk_user_role` (`user_role_id`);
-
---
--- Indexes for table `user_profile`
---
-ALTER TABLE `user_profile`
-  ADD PRIMARY KEY (`user_id`),
-  ADD UNIQUE KEY `display_name` (`display_name`),
-  ADD KEY `galaxy_name` (`galaxy_name`),
-  ADD KEY `solar_sys_name` (`solar_sys_name`),
-  ADD KEY `planet_name` (`planet_name`),
-  ADD KEY `galaxy_name_2` (`galaxy_name`,`solar_sys_name`),
-  ADD KEY `galaxy_name_3` (`galaxy_name`,`solar_sys_name`,`planet_name`);
-
---
--- Indexes for table `user_role`
---
-ALTER TABLE `user_role`
-  ADD PRIMARY KEY (`user_role_id`),
-  ADD UNIQUE KEY `role_desc` (`role_desc`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `user_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'Primary key', AUTO_INCREMENT=23;
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `user`
---
-ALTER TABLE `user`
-  ADD CONSTRAINT `user_fk_user_role` FOREIGN KEY (`user_role_id`) REFERENCES `user_role` (`user_role_id`) ON UPDATE CASCADE;
-
---
--- Constraints for table `user_profile`
---
-ALTER TABLE `user_profile`
-  ADD CONSTRAINT `user_profile_fk_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
-COMMIT;
-
-Create View `get_profile` As
-SELECT `user_id`, `full_name`, `email`, `passwd`, `salt`, `role_desc`, `display_name`, `tagline`, `bio`, `profile_photo`, `galaxy_name`, `solar_sys_name`, `planet_name`
-FROM `user`
-INNER JOIN `user_profile` USING(`user_id`)
-INNER JOIN `user_role` ON `user`.`user_role_id` = `user_role`.`user_role_id`;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
